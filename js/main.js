53245828b8ba60f70bd0f4268c0a14a9d50294e2
pierfrancescoscar <scarinci401@gmail.com>
2021-10-20 15:42:06 +0200

    add js user question and his command result

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Asking user's name
+
+const firstName = prompt ('What is your name?');
+
+// Asking user's last-name
+
+const lastName = prompt ('What is your surname?');
+
+// Asking user's favorite color
+
+const favCol = prompt ('What is your favorite color?')
+
+// Show result
+
+document.getElementById('result').innerHTML = 'Your password is ' + firstName + lastName + favCol + '21';
